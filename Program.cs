8aa5c48c53135ef37b3165983e37186019ef2e2d
Dustin Savage <Dnsavage@crimson.ua.edu>
2021-11-19 21:05:11 -0600

    Completed lab

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Composition_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Author myAuthor = new Author("Dustin Savage", 'm', "dnsavage@crimson.ua.edu");
+            Console.WriteLine(myAuthor.ToString() + "\n");
+
+            Book myBook = new Book("12243", "My Book, Your Book", myAuthor);
+            Console.WriteLine(myBook.ToString() + "\n");
+
+            Console.WriteLine(myBook.GetAuthor().ToString() + "\n");
+
+            Book yourBook = new Book("1997", "A Transparent Book", new Author("jamie", 'f', "jamie@mail.com"));
+            Console.WriteLine(yourBook.ToString());
+
+            yourBook.GetAuthor().SetEmail("jamie@crimson.ua.edu");
+
+            Console.WriteLine(yourBook.ToString());
+        }
+    }
+}
